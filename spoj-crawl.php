9e6c4cb5a9aee0d514f854b6c5420030fe85e493
Abhiram Ravikumar <abhi12ravi@gmail.com>
2016-04-06 23:26:35 +0530

    Adds php web crawler

--- spoj-crawl.php
+++ spoj-crawl.php
@@ -1,5 +1,5 @@
 <?php
-2. $url = 'http://www.oooff.com';
-3. $output = file_get_contents($url); 
-4. echo $output;
-5. ?>
+$url = 'http://www.oooff.com';
+$output = file_get_contents($url); 
+echo $output;
+?>
