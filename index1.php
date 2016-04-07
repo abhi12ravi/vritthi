486388125306274cfeee7ea315f2341b5798123f
Abhiram Ravikumar <abhi12ravi@gmail.com>
2016-04-08 00:08:07 +0530

    Adding SQL functionality

--- index1.php
+++ index1.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
+<html lang="en">
+
 <?php
 
 require 'db-functions.php';
 session_start();
 
 ?>
-<html lang="en">
+
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
