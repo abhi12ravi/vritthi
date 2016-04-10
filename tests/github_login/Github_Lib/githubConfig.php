118decb9f578f721b3fe367ab58c7aebc03a6879
Abhiram Ravikumar <abhi12ravi@gmail.com>
2016-04-10 21:13:42 +0530

    Github login trial 1

---
+++ tests/github_login/Github_Lib/githubConfig.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * All Config necessarry
+ */
+$config = array('client_id' => '814d73883b6f03e84fd1',							//  Github client ID
+				'client_secret' => '2aa5f1769d14bfb8b693aaddf836cf3e99434670',	//  Github client SECRET
+				'redirect_url' => 'http://vritthi-abhi12ravi.rhcloud.com/tests/github_login/github_login.php',	//  Github Redirect URL (As mentioned in GITHUB APP PAGE)
+				'app_name' => 'Vritthi');								//  Github App Name (As mentioned in GITHUB APP PAGE)
+?>
---
+++ tests/github_login/db.php
@@ -0,0 +1,8 @@
+<?php
+$servername = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$username = "adminDPUepnP";
+$password = "38E1d5whUcQE";
+$dbname = "vritthi";
+$connection = @mysqli_connect($servername,$username,$password,$dbname);
+
+?>
